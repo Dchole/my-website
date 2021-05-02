d3b876f893b66b26587bd1034dac19fdca3bde67
Dchole <derekoware47@gmail.com>
2021-05-02 18:49:30 +0000

    Removed jit mode from tailwind config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 import Layout from "@/components/Layout";
+
+import "tailwindcss/tailwind.css";
 import "@fontsource/nunito-sans/600.css";
 import "@fontsource/nunito-sans/700.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/400-italic.css";
-
-import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
