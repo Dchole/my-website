5c512d4faf74c81bf1dfb8e3709cea5a271b2fcd
Dchole <derekoware47@gmail.com>
2021-06-11 09:41:01 +0000

    project details

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from "next/head"
 
 const About = () => {
   return (
@@ -15,20 +15,17 @@ const About = () => {
           I’m a full-stack web developer with love and enthusiasm for creating
           and experimenting with modern web technologies. I specialize mostly on
           frontend technologies specifically in the React ecosystem and also
-          come along with web design writing in my skill set
+          come along with <em>web design</em> and <em>writing</em> in my skill
+          set
         </p>
         <p className="mt-4 text-gray-600 dark:text-gray-300 font-body tracking-body">
           I’ve been a developer for about 3 years and it has been a fulfilling
           experience. I’ve started a blog on dev.to and hashnode to share my
           experience and tips I find on my journey with the dev community.
         </p>
-        <p className="mt-4 text-gray-600 dark:text-gray-300 font-body tracking-body">
-          I started my journey with HTML5 canvas and then jumped into React.
-          I've been in the React ecosystem ever since.
-        </p>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default About;
+export default About
