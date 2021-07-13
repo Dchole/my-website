4b79d58779b03b1a4b2d32a920dddc152ef402dd
Dchole <derekoware47@gmail.com>
2021-07-13 13:43:25 +0000

    pinterest verify

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,18 @@
-import App from "next/app";
-import Head from "next/head";
-import Layout from "@/components/Layout";
+import App from "next/app"
+import Head from "next/head"
+import Layout from "@/components/Layout"
 
-import "@fontsource/nunito-sans/600.css";
-import "@fontsource/nunito-sans/700.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/400-italic.css";
-import "@fontsource/fira-code/400.css";
-import "tailwindcss/tailwind.css";
+import "@fontsource/nunito-sans/600.css"
+import "@fontsource/nunito-sans/700.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/400-italic.css"
+import "@fontsource/fira-code/400.css"
+import "tailwindcss/tailwind.css"
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps } = this.props
 
     return (
       <Layout>
@@ -21,11 +21,15 @@ class MyApp extends App {
             name="description"
             content="I’m a full-stack web developer with love and enthusiasm for creating and experimenting with modern web technologies. I specialize mostly on frontend technologies specifically in the React ecosystem"
           />
+          <meta
+            name="p:domain_verify"
+            content="bca0c5badbbfd7ccf646b038970cfc48"
+          />
         </Head>
         <Component {...pageProps} />
       </Layout>
-    );
+    )
   }
 }
 
-export default MyApp;
+export default MyApp
